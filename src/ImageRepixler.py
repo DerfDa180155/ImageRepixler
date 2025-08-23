2e6aa3687eed94c30d46e4497f508a1fd9cd4a35
DerfDa180155 <derflinger.david@gmail.com>
2025-08-23 23:44:43 +0200

    added initial code

---
+++ src/ImageRepixler.py
@@ -0,0 +1,11 @@
+import numpy
+
+class ImageRepixler:
+    def __init__(self):
+        pass
+
+    def loadColors(self):
+        pass
+
+    def repixel(self):
+        pass
---
+++ src/Main.py
@@ -0,0 +1,16 @@
+import pygame
+
+import ImageRepixler
+
+
+class main:
+    def __init__(self):
+        self.imagerepixler = ImageRepixler.ImageRepixler()
+        self.run()
+
+    def run(self):
+        pass
+
+
+if __name__ == "__main__":
+    main()
